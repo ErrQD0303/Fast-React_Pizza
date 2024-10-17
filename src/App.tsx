252f723a5572099a2af76b75ffebea966acc6e21
ErrQD0303 <quocdat3396@gmail.com>
2024-10-17 23:24:00 +0700

    first push

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./layouts/Home.tsx";
+import Cart from "./features/cart/Cart.tsx";
+import Menu from "./features/menu/Menu.tsx";
+import CreateOrder from "./features/order/CreateOrder.tsx";
+import Order from "./features/order/Order.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
